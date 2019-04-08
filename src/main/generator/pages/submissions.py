87c143daf8a9b3177df13e09f783a41c70a39c3c
Zac Hayes <zrev2220@gmail.com>
2019-04-07 23:01:55 -0400

    Complete contestant submission table (need to remove commented code in submissions.py:33)

--- src/main/generator/pages/submissions.py
+++ src/main/generator/pages/submissions.py
@@ -5,30 +5,48 @@ from code.util import register
 from code.generator.pages.judge import verdict_name, icons
 import logging
 
-class SubmissionDisplay(UIElement):
+class SubmissionCard(UIElement):
     def __init__(self, submission: Submission):
         subTime = submission.timestamp
         probName = submission.problem.title
         cls = "gray" if submission.status == "Review" else "red" if submission.result != "ok" else ""
-        self.html = Card("Submission to {} at <span class='time-format'>{}</span>".format(probName, subTime), [
-            h.strong("Language: <span class='language-format'>{}</span>".format(submission.language)),
-            h.br(),
-            h.strong("Result: <span class='result-format'>{}</span>".format(verdict_name[submission.getContestantResult()])),
-            h.br(),
-            h.br(),
-            h.strong("Code:"),
-            h.code(submission.code.replace("\n", "<br/>").replace(" ", "&nbsp;"))
-        ], cls=cls)
+        self.html = div(cls="modal-content", contents=[
+            div(cls=f"modal-header {cls}", contents=[
+                h.h5(
+                    f"Submission to {probName} at ",
+                    h.span(subTime, cls="time-format")
+                ),
+                """
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>"""
+            ]),
+            div(cls="modal-body", contents=[
+                h.strong("Language: <span class='language-format'>{}</span>".format(submission.language)),
+                h.br(),
+                h.strong("Result: "),
+                verdict_name[submission.getContestantResult()],
+                h.br(),
+                h.br(),
+                h.strong("Code:"),
+                h.code(submission.code.replace("\n", "<br/>").replace(" ", "&nbsp;"), cls="code"),
+                # div(cls="result-tabs", id="result-tabs", contents=[
+                #     h.ul(*map(lambda x: TestCaseTab(x, submission), enumerate(submission.results))),
+                #     *map(lambda x: TestCaseData(x, submission), zip(range(submission.problem.tests), submission.inputs, submission.outputs, submission.errors, submission.answers))
+                # ])
+            ])
+        ])
 
 class SubmissionRow(UIElement):
     def __init__(self, sub):
+        result = sub.getContestantResult()
         self.html = h.tr(
             h.td(sub.problem.title),
             h.td(cls='time-format', contents=sub.timestamp),
             h.td(sub.language),
             h.td(
-                h.i("&nbsp;", cls=f"fa fa-{icons[sub.result]}"),
-                h.span(verdict_name[sub.result])
+                h.i("&nbsp;", cls=f"fa fa-{icons[result]}"),
+                h.span(verdict_name[result])
             ),
             onclick=f"submissionPopupContestant('{sub.id}')"
         )
@@ -50,44 +68,6 @@ class SubmissionTable(UIElement):
             id="mySubmissions"
         )
 
-class SubmissionCard(UIElement):
-    def __init__(self, submission: Submission):
-        subTime = submission.timestamp
-        probName = submission.problem.title
-        cls = "red" if submission.result != "ok" else ""
-        self.html = div(cls="modal-content", contents=[
-            div(cls=f"modal-header {cls}", contents=[
-                h.h5(
-                    f"Submission to {probName} at ",
-                    h.span(subTime, cls="time-format")
-                ),
-                """
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>"""
-            ]),
-            div(cls="modal-body", contents=[
-                h.strong("Language: <span class='language-format'>{}</span>".format(submission.language)),
-                # h.br(),
-                # h.strong("Result: ",
-                #     h.select(cls=f"result-choice {submission.id}", onchange=f"changeSubmissionResult('{submission.id}')", contents=[
-                #         *resultOptions(submission.result)
-                #     ])
-                # ),
-                # h.br(),
-                # h.br(),
-                # h.button("Rejudge", type="button", onclick=f"rejudge('{submission.id}')", cls="btn btn-primary rejudge"),
-                h.br(),
-                h.br(),
-                h.strong("Code:"),
-                h.code(submission.code.replace("\n", "<br/>").replace(" ", "&nbsp;"), cls="code"),
-                # div(cls="result-tabs", id="result-tabs", contents=[
-                #     h.ul(*map(lambda x: TestCaseTab(x, submission), enumerate(submission.results))),
-                #     *map(lambda x: TestCaseData(x, submission), zip(range(submission.problem.tests), submission.inputs, submission.outputs, submission.errors, submission.answers))
-                # ])
-            ])
-        ])
-
 def getSubmissions(params, user):
     submissions = []
     
@@ -102,11 +82,17 @@ def getSubmissions(params, user):
         )
     return Page(
         h2("Your Submissions", cls="page-title"),
-        SubmissionTable(submissions)
+        SubmissionTable(submissions),
+        div(cls="modal", tabindex="-1", role="dialog", contents=[
+            div(cls="modal-dialog", role="document", contents=[
+                div(id="modal-content")
+            ])
+        ])
     )
 
 def contestant_submission(params, user):
+    print(SubmissionCard(Submission.get(params[0])))
     return SubmissionCard(Submission.get(params[0]))
 
 register.web("/submissions", "loggedin", getSubmissions)
-register.web("/contestantSubmission/([a-zA-Z0-9-]*)", "admin", contestant_submission)
+register.web("/contestantSubmission/([a-zA-Z0-9-]*)", "loggedin", contestant_submission)
